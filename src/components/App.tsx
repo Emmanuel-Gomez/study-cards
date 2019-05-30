3c108f0962a5138028959187358334ca397ae385
Emmanuel Gomez <emmanuel.gomez@lakarleasing.se>
2019-05-30 15:00:04 +0200

    First commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import '../style/App.css';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<h1>Study Cards</h1>
+			</div>
+		);
+	}
+}
